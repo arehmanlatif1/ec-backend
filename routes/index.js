c67f47e973f0c2cf01e5da7e7202b63589a37fda
arehmanlatif1 <rehmanlatif335@gmail.com>
2024-12-24 12:37:58 -0500

    make it better

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,3 @@
-// import express from "express";
-// import productsRoute from "./productsRoute.js"
-// import cartRoute from "./cartRoute.js";
-// import ordersRoute from "./ordersRoute.js";
-
-
-// const router = express.Router();
-
-// // Use the individual route files
-// router.use("/products", productsRoute);
-// router.use("/cart", cartRoute);
-// router.use("/orders", ordersRoute);
-
-// export default router;
 import { Router } from "express";
 import productsRoutes from "./productsRoute.js";
 import usersRoutes from "./usersRoute.js";
