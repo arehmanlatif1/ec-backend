0a52ce9cf00e5c1285d73cb0c17df066c84c337a
arehmanlatif1 <rehmanlatif335@gmail.com>
2025-01-01 14:51:24 -0500

    get users

--- controllers/users.js
+++ controllers/users.js
@@ -1,394 +1,114 @@
-import User from "../models/User.js";
 import bcrypt from "bcrypt";
 import jwt from "jsonwebtoken";
+import User from "../models/User.js";
 
-// for development purposes
 let SALT_ROUNDS = 11;
-let TOKEN_KEY = "areallylonggoodkey";
+let TOKEN_KEY = "reallylonggoodkey";
 
-// for production
 if (process.env.NODE_ENV === "production") {
   SALT_ROUNDS = Number(process.env.SALT_ROUNDS);
   TOKEN_KEY = process.env.TOKEN_KEY;
 }
 
-// for JWT expiration
 const today = new Date();
 const exp = new Date(today);
 exp.setDate(today.getDate() + 30);
 
-//posts
-export const registerUser = async (req, res) => {
-  const { firstName, lastName, dateOfBirth, email, password } = req.body;
-  let user;
-
+export const signUp = async (req, res) => {
   try {
-    // check if user already exists
-    user = await User.findOne({ email });
-    if (user) {
-      return res.status(400).json({ error: "User already exists" });
-    }
+    const { userName, email, password } = req.body;
 
-    // hash
-    const salt = await bcrypt.genSalt(10);
-    const hashedPassword = await bcrypt.hash(password, salt);
+    const password_digest = await bcrypt.hash(password, SALT_ROUNDS);
 
-    // create new user
-    user = new User({
-      firstName,
-      lastName,
+    const user = new User({
+      userName,
       email,
-      password: hashedPassword,
-      dateOfBirth,
-      favorites: [],
-      shippingAddress: {
-        streetAddress: "",
-        streetAddress2: "",
-        city: "",
-        state: "",
-        postalCode: "",
-      },
+      password_digest,
+      favGames: []
     });
+    console.log(user)
 
     await user.save();
-
+    
     const payload = {
-      id: user.id,
-      firstName: user.firstName,
-      lastName: user.lastName,
+      id: user._id,
+      userName: user.userName,
       email: user.email,
-      dateOfBirth: user.dateOfBirth,
-      favorites: user.favorites,
       exp: parseInt(exp.getTime() / 1000),
     };
 
     const token = jwt.sign(payload, TOKEN_KEY);
-    res.status(201).json({ message: "User registered successfully", token });
+
+    res.status(201).json({ token });
   } catch (error) {
-    console.error(error.message);
+    console.log(error.message);
     res.status(400).json({ error: error.message });
   }
 };
-export const loginUser = async (req, res) => {
+
+export const signIn = async (req, res) => {
   try {
     const { email, password } = req.body;
-    // select() is used to explicitly select which field will be returned
-    const user = await User.findOne({ email }).select(
-      "firstName lastName dateOfBirth email password favorites"
-    );
-    if (!user) {
-      return res.status(404).json({ message: "Invalid Email or Password" });
-    }
 
-    if (await bcrypt.compare(password, user.password)) {
+    const user = await User.findOne({ email: email }).select(
+      "userName email password_digest favGames"
+    );
+
+    if (await bcrypt.compare(password, user.password_digest)) {
       const payload = {
-        id: user.id,
-        firstName: user.firstName,
-        lastName: user.lastName,
+        id: user._id,
+        userName: user.userName,
         email: user.email,
-        dateOfBirth: user.dateOfBirth,
-        favorites: user.favorites,
+        favGames: user.favGames,
         exp: parseInt(exp.getTime() / 1000),
       };
 
       const token = jwt.sign(payload, TOKEN_KEY);
+
       res.status(201).json({ token });
     } else {
-      res.status(401).json({
-        message: "User registered successfully",
-        error: "Invalid Credentials",
-      });
-    }
-  } catch (error) {
-    console.error(error.message);
-    res.status(400).json({ error: error.message });
-  }
-};
-// gets
-export const getUsers = async (req, res) => {
-  try {
-    let users = await User.find();
-    res.json(users);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
-};
-export const getUserById = async (req, res) => {
-  try {
-    const id = req.params.id;
-    const user = await User.findById(id);
-    if (!user) {
-      return res.status(404).json({ message: "Invalid User ID" });
-    }
-    res.json(user);
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
-};
-//delete user
-export const deleteUserById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const user = await User.findByIdAndDelete(id);
-
-    if (!user) {
-      return res.status(404).json({ message: "Invalid ID" });
-    }
-
-    res.json({ message: "User deleted successfully" });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
-};
-//put <3
-export const updateUserFavoritesById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { favorites: favorite } = req.body;
-
-    if (!favorite) {
-      return res.status(400).json({ message: "Favorite Wine not provided" });
-    }
-
-    const wine = await User.findByIdAndUpdate(
-      id,
-      { $push: { favorites: favorite } },
-      { new: true }
-    );
-
-    if (!wine) {
-      return res.status(404).json({ message: "Invalid ID" });
-    }
-
-    res.json({ message: "Wine favorites updated successfully", wine });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
-};
-
-export const deleteUserFavoritesById = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { favorites: favorite } = req.body;
-
-    if (!favorite) {
-      return res.status(400).json({ message: "Favorite Wine not provided" });
-    }
-
-    const wine = await User.findByIdAndUpdate(
-      id,
-      { $pull: { favorites: favorite } },
-      { new: true }
-    );
-
-    if (!wine) {
-      return res.status(404).json({ message: "Invalid ID" });
-    }
-
-    res.json({ message: "Wine favorites removed successfully", wine });
-  } catch (error) {
-    console.log(error);
-    res.status(500).json({ error: error.message });
-  }
-};
-//verify
-export const verify = async (req, res) => {
-  try {
-    // Check if the authorization header exists
-    if (req.headers.authorization) {
-      const token = req.headers.authorization.split(" ")[1];
-      const payload = jwt.verify(token, TOKEN_KEY);
-      if (payload) {
-        res.json(payload);
-      }
-    } else {
-      // Handle the case where the authorization header is missing
-      res.status(401).send("Authorization header missing");
+      res.status(401).send("Invalid credentials");
     }
   } catch (error) {
     console.log(error.message);
-    res.status(401).send("Not Authorized");
+    res.status(500).json({ error: error.message });
   }
 };
 
+export const verify = async (req, res) => {
+  try {
+    const token = req.headers.authorization.split(" ")[1];
 
+    const payload = jwt.verify(token, TOKEN_KEY);
+    if (payload) {
+      res.json(payload);
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(401).send("Not authorized");
+  }
+};
 
+export const getUsers = async (req, res) => {
+  try {
+    const users = await User.find();
+    res.json(users);
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
 
-
-
-// import bcrypt from "bcrypt";
-// import jwt from "jsonwebtoken";
-// import User from "../models/User.js";
-
-// let SALT_ROUNDS = 11;
-// let TOKEN_KEY = "reallylonggoodkey";
-
-// if (process.env.NODE_ENV === "production") {
-//   SALT_ROUNDS = Number(process.env.SALT_ROUNDS);
-//   TOKEN_KEY = process.env.TOKEN_KEY;
-// }
-
-// const today = new Date();
-// const exp = new Date(today);
-// exp.setDate(today.getDate() + 30);
-
-// export const signUp = async (req, res) => {
-//   try {
-//     const { userName, email, password } = req.body;
-
-//     const password_digest = await bcrypt.hash(password, SALT_ROUNDS);
-
-//     const user = new User({
-//       userName,
-//       email,
-//       password_digest,
-//       cartProducts: [] // Change from favGames to cartProducts
-//     });
-
-//     await user.save();
-
-//     const payload = {
-//       id: user._id,
-//       userName: user.userName,
-//       email: user.email,
-//       exp: parseInt(exp.getTime() / 1000),
-//     };
-
-//     const token = jwt.sign(payload, TOKEN_KEY);
-
-//     res.status(201).json({ token });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(400).json({ error: error.message });
-//   }
-// };
-
-// export const signIn = async (req, res) => {
-//   try {
-//     const { email, password } = req.body;
-
-//     const user = await User.findOne({ email: email }).select(
-//       "userName email password_digest cartProducts"
-//     );
-
-//     if (await bcrypt.compare(password, user.password_digest)) {
-//       const payload = {
-//         id: user._id,
-//         userName: user.userName,
-//         email: user.email,
-//         exp: parseInt(exp.getTime() / 1000),
-//       };
-
-//       const token = jwt.sign(payload, TOKEN_KEY);
-
-//       res.status(201).json({ token });
-//     } else {
-//       res.status(401).send("Invalid credentials");
-//     }
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// export const verify = async (req, res) => {
-//   try {
-//     const token = req.headers.authorization.split(" ")[1];
-
-//     const payload = jwt.verify(token, TOKEN_KEY);
-//     if (payload) {
-//       res.json(payload);
-//     }
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(401).send("Not authorized");
-//   }
-// };
-
-// export const getUsers = async (req, res) => {
-//   try {
-//     const users = await User.find();
-//     res.json(users);
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// export const getUser = async (req, res) => {
-//   try {
-//     const { id } = req.params;
-//     const user = await User.findById(id);
-//     if (user) {
-//       return res.json(user);
-//     }
-//     res.status(404).json({ message: "User not found!" });
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// export const addCartProduct = async (req, res) => {
-//   try {
-//     const { productId } = req.params;
-
-//     const token = req.headers.authorization.split(" ")[1];
-//     const payload = jwt.verify(token, TOKEN_KEY);
-
-//     if (payload) {
-//       await User.findByIdAndUpdate(payload.id, { $push: { cartProducts: productId } });
-//       res.json({ message: `Product with id of ${productId} has been added to User's cart` });
-//     } else {
-//       res.json({ message: "Unauthorized" });
-//     }
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// export const deleteCartProduct = async (req, res) => {
-//   try {
-//     const { productId } = req.params;
-
-//     const token = req.headers.authorization.split(" ")[1];
-//     const payload = jwt.verify(token, TOKEN_KEY);
-
-//     if (payload) {
-//       const user = await User.findById(payload.id);
-
-//       if (user) {
-//         user.cartProducts = user.cartProducts.filter((id) => id.toString() !== productId);
-//         await user.save();
-//         res.json({ message: `Product with id of ${productId} has been deleted from User's cart` });
-//       } else {
-//         res.status(404).json({ message: "User not found!" });
-//       }
-//     } else {
-//       res.json({ message: "Unauthorized" });
-//     }
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).json({ error: error.message });
-//   }
-// };
-
-// export const getCartProducts = async (req, res) => {
-//   try {
-//     const token = req.headers.authorization.split(" ")[1];
-
-//     const payload = jwt.verify(token, TOKEN_KEY);
-//     if (payload) {
-//       const user = await User.findById(payload.id).populate("cartProducts"); // Populate the products if you're referencing Product model
-//       res.json(user.cartProducts);
-//     }
-//   } catch (error) {
-//     console.log(error.message);
-//     res.status(500).json({ error: error.message });
-//   }
-// };
+export const getUserById = async (req, res) => {
+  try {
+    const { id } = req.params;
+    const user = await User.findById(id);
+    if (user) {
+      return res.json(user);
+    }
+    res.status(404).json({ message: "User not found!" });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).json({ error: error.message });
+  }
+};
--- routes/usersRoute.js
+++ routes/usersRoute.js
@@ -1,16 +1,16 @@
 import { Router } from "express";
-import * as controllers from "../controllers/users.js";
+import { signUp, signIn, verify, getUsers, getUserById } from "../controllers/users.js";
 
 const router = Router();
 
-router.post("/register", controllers.registerUser);
-router.post("/login", controllers.loginUser);
-router.get("/", controllers.getUsers);
-router.get("/id/:id", controllers.getUserById);
-router.delete("/:id", controllers.deleteUserById);
-router.patch("/:id/favorites", controllers.updateUserFavoritesById);
-router.patch("/delete/:id/favorites", controllers.deleteUserFavoritesById);
-router.get("/verify", controllers.verify);
+router.post("/sign-up", signUp);
+router.post("/sign-in", signIn);
+router.get("/verify", verify);
+
+router.get("/", getUsers);
+router.get("/:id", getUserById);
+// router.delete("/:id", controllers.deleteUserById);
+
 
 export default router;
 
